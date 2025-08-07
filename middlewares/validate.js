bb512592daebe78de485d73cabaf735c5657de5f
Sigma wadbude <sigmawadbude@gmail.com>
2025-08-07 13:00:17 -0400

    updated movie schema

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,6 +1,6 @@
 module.exports = (schema) => (req, res, next) => {
   const result = schema.safeParse(req.body);
-
+  
   if (!result.success) {
     return res.status(400).json({
       status: 'failure',
--- models/Movie.js
+++ models/Movie.js
@@ -3,12 +3,25 @@ const mongoose = require('mongoose');
 const movieSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true,
+    required: [true, 'Title is required'],
+    trim: true,
   },
-  year: {
+  director: {
+    type: String,
+    required: [true, 'Director is required'],
+    trim: true,
+  },
+  releaseYear: {
     type: Number,
-    required: true,
+    required: [true, 'Release year is required'],
+    min: [1900, 'Year too early'],
+    max: [new Date().getFullYear(), 'Year in the future'],
   },
-});
+  genre: {
+    type: String,
+    required: [true, 'Genre is required'],
+    trim: true,
+  },
+}, { timestamps: true });
 
 module.exports = mongoose.model('Movie', movieSchema);
